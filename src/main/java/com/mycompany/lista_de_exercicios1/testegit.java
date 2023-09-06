b26cac545d18aae50f7b48b18b06e4214383fb80
BRIAN FELIPE GONCALVES <brian.7908@aluno.pr.senac.br>
2023-09-06 14:45:37 -0300

    testeadicionado

---
+++ src/main/java/com/mycompany/lista_de_exercicios1/testegit.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lista_de_exercicios1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author brian.7908
+ */
+public class testegit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+    }    
+}
