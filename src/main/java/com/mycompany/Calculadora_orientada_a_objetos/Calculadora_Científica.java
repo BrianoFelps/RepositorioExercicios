acb8122d703056c84cd620a7084eac6a5bddd515
BRIAN FELIPE GONCALVES <brian.7908@aluno.pr.senac.br>
2023-09-13 16:00:32 -0300

    +calculadoracientifica

--- src/main/java/com/mycompany/Calculadora_orientada_a_objetos/Calculadora_Científica.java
+++ src/main/java/com/mycompany/Calculadora_orientada_a_objetos/Calculadora_Científica.java
@@ -8,6 +8,16 @@ package com.mycompany.Calculadora_orientada_a_objetos;
  *
  * @author brian.7908
  */
-public class Calculadora_Científica {
-    
+
+//"extends" Tem acesso a todas as propriedades da classe calculadora, e os novos metodos a serem implementados serão referentes somente à científica.
+public class Calculadora_Científica extends Calculadora {
+    //espera-se receber um double a, e sem colocar return dá erro.
+    public Double raizQuadrada(Double a) {
+        //o math sqrt representa a raíz quadrada.    
+        return Math.sqrt(a);
+    }
+    public Double Potencia (Double a, Double b){
+    //math.pow potencia.    
+    return Math.pow(a, b);
+    }
 }
--- src/main/java/com/mycompany/Calculadora_orientada_a_objetos/ProgramaCalculadora.java
+++ src/main/java/com/mycompany/Calculadora_orientada_a_objetos/ProgramaCalculadora.java
@@ -13,8 +13,10 @@ import java.util.Scanner;
 public class ProgramaCalculadora {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora ();        
+        Calculadora_Científica cc = new Calculadora_Científica();
         
         float soma, sub, mult, div, num1, num2 ;
+        Double RQ, Pot, num3, num4, num5;
         
         Scanner scanner = new Scanner (System.in);
         
@@ -24,15 +26,38 @@ public class ProgramaCalculadora {
         System.out.println("Informe o segundo número: ");
         num2 = scanner.nextFloat();
         
-        soma = calculadora.somar(num1, num2);
-        sub= calculadora.subtrair(num1, num2);
-        mult = calculadora.multiplicar(num1, num2);
-        div = calculadora.dividir(num1, num2);
+        soma = cc.somar(num1, num2);
+        sub= cc.subtrair(num1, num2);
+        mult = cc.multiplicar(num1, num2);
+        div = cc.dividir(num1, num2);
+     
         
+        //Soma, Subtração, Multiplicação e Divisão
         System.out.println("Resultado da soma: " + soma);
         System.out.println("Resultado da subtração: " + sub);
         System.out.println("Resultado da multiplicação: " + mult);
         System.out.println("Resultado da divisão: " + div);
-    
+        
+        //Raíz quadrada
+        System.out.println("------Raíz quadrada------");
+                System.out.println("Agora, informe um número par para extrair a raíz quadrada: ");
+            num3 = scanner.nextDouble();
+            
+               RQ = cc.raizQuadrada(num3);
+               
+               System.out.println("A raíz quadrada de " + num3 + " é: " + RQ);
+         //Potência
+         System.out.println("------Potência------");
+         
+                         System.out.println("Agora, informe o número para potencializar: ");
+            num4 = scanner.nextDouble();
+            
+        System.out.println("E a potencia:");
+            num5 = scanner.nextDouble();
+            
+            Pot = cc.Potencia(num4, num5);
+            
+            System.out.println("O número " + num4 + " elevado a " + num5 + " resulta em: " + Pot);
+        
     }
 }
